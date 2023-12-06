3bdf9988a6f98c9c5060c473daf1bc0816efaac9
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-06 18:22:54 +0100

    java_1_1

---
+++ javascript_intro/assets/js/Console-Level-1_1.js
@@ -0,0 +1,32 @@
+// 1. Beispiel
+
+let a = 12;
+let b = a * 3;
+console.log(b);
+
+console.log("Hello World!");
+
+const firstName = "Anton";
+let age = 28
+console.log(firstName);
+console.log(age);
+
+let job = "Trainer:in";
+let married = "true"
+console.log(job);
+console.log(married);
+
+console.log(firstName + " ist " + age + " jahre alt, von Beruf " + job + " und ist verheiratet: " + married );
+
+
+document.querySelector('#output').innerHTML = b
+
+
+
+
+
+
+
+
+
+
