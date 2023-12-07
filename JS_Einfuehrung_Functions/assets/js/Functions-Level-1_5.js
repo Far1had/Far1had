cc39d3ed23caac9f0d9fde0248f8bfff556e6792
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-07 15:43:24 +0100

    Functions-Level-1_5

---
+++ JS_Einfuehrung_Functions/assets/js/Functions-Level-1_5.js
@@ -0,0 +1,12 @@
+function math(param1, param2) {
+    console.log(param1 * param2);
+    console.log(param1 / param2);
+}
+
+math(30, 20)
+math (100, 100)
+math (5, 0)
+math (45, 173)
+math (1, 1000)
+math(3, 2)
+math (2, 3)
