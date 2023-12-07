d47caf3da63fe567f241cee53fb145c98d2e60a1
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-07 15:19:38 +0100

    Functions-Level-1_3

---
+++ JS_Einfuehrung_Functions/assets/js/Functions-Level-1_3.js
@@ -0,0 +1,4 @@
+function intro3(name, stadt, alter){
+    console.log(`Hallo, mein name ist ${name} ich bin ${alter} jahre alt. Ich komme aus  ${stadt}`);
+}
+intro3("SuperCoder", "Düsseldorf", 27);
