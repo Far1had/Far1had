f4b2d2dabba618c0206fe8061de25b9e898069b8
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-07 18:47:07 +0100

    Functions-Level-3_1

---
+++ JS_Einfuehrung_Functions/assets/js/Functions-Level-3_1.js
@@ -0,0 +1,14 @@
+
+
+function changeColors(bgColor, textColor) {
+    let body = document.body;
+    let title = document.getElementById('title');
+
+    body.style.background = bgColor;
+    title.style.color = textColor;
+}
+
+//ChangeColors ("red", "blue")
+//changeColors ("red", "white")
+//changeColors ("white", "red")
+
