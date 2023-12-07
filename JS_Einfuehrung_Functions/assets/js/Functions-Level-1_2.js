31f558ed49ee3aa7647c999913bb60eed2883924
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-07 15:03:39 +0100

    Functions-Level-1_2

---
+++ JS_Einfuehrung_Functions/assets/js/Functions-Level-1_2.js
@@ -0,0 +1,10 @@
+function intro2(paramName) {
+    let name = "SuperCoder:in";
+    console.log("Hi " + name + ". Mein Name ist " + paramName + ".");
+}
+
+// Beispielaufrufe der Funktion mit verschiedenen Argumenten
+intro2("Michael");
+intro2("Faer had");
+intro2("Boby");
+intro2("Charls");
