152d995e70b8af6ea87fbc34da830a62bba8b7ac
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-08 15:36:16 +0100

    Forms-JS-Level-1_1

--- JS_Einführung_Forms/assets/js/Forms-JS-Level-1_1.js
+++ JS_Einführung_Forms/assets/js/Forms-JS-Level-1_1.js
@@ -1,7 +1,7 @@
 function verdoppeln() {
 
-    var inputValue = document.getElementById('num-id').value;
-    var doubledValue = parseFloat(inputValue) * 2 || 0;
+    let inputValue = document.getElementById('num-id').value;
+    let doubledValue = parseFloat(inputValue) * 2 || 0;
 
     document.getElementById('result').innerText = 'Das verdoppelte Ergebnis ist: ' + doubledValue;
 }
