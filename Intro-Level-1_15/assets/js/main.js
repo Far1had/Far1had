9a47a640ae981cf677f9399b40b5eab8e79765aa
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-08 14:29:15 +0100

    Intro-Level-1_15-first commit

---
+++ Intro-Level-1_15/assets/js/main.js
@@ -0,0 +1,20 @@
+let  num = 5;
+let anotherNum = 8;
+console.log(num == anotherNum);
+console.log(num > anotherNum);
+console.log(num < anotherNum);
+console.log(num >= anotherNum);
+console.log(num <= anotherNum);
+console.log(num == "5");
+console.log(num != 8);
+console.log(num != "5");
+
+/*
+main.js:3 false
+main.js:4 false
+main.js:5 true
+main.js:6 false
+main.js:7 true
+main.js:8 true
+main.js:9 true
+main.js:10 */
