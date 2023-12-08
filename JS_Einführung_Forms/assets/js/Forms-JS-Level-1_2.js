17addc8a5a79b0aec0fdc2d2ff0f3ce4b9e4052b
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-08 17:51:03 +0100

    Forms-JS-Level-1_2-final

--- JS_Einführung_Forms/assets/js/Forms-JS-Level-1_2.js
+++ JS_Einführung_Forms/assets/js/Forms-JS-Level-1_2.js
@@ -1,1 +1,9 @@
+function chinchin() {
+
+    let eingabeWert = document.getElementById('bday').value;
+    const aktuellesJahr = new Date().getFullYear();
+    const doppeln = aktuellesJahr - eingabeWert;
+
+    document.getElementById('result').innerText = 'Du bist erst ' + doppeln + ' Jahre jung. <3';
+}
 
