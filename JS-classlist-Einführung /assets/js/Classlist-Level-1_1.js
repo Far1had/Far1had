bae25af0ae6f4380a974d90ef6323a097500f5c4
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-08 20:04:00 +0100

    Classlist-Level-1_1

---
+++ JS-classlist-Einführung /assets/js/Classlist-Level-1_1.js
@@ -0,0 +1,9 @@
+function headlineh1() {
+    let headlineElement = document.getElementById("head-id");
+    headlineElement.classList.toggle("head-class");
+}
+
+function chinchin() {
+    let wishesElement = document.getElementById("wishes");
+    wishesElement.classList.toggle("list-class");
+}
