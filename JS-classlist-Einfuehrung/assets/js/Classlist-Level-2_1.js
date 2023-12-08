16e4be9c57866827b583ae5cf6e5d778fde4dddf
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-08 21:36:24 +0100

    Classlist-Level-2_1-finale

--- JS-classlist-Einfuehrung/assets/js/Classlist-Level-2_1.js
+++ JS-classlist-Einfuehrung/assets/js/Classlist-Level-2_1.js
@@ -3,36 +3,23 @@ function headShow() {
     headShow.classList.toggle("head-class");
 }
 
-function wishes() {
-    let wishesElement = document.getElementById("wishes");
-    wishesElement.classList.toggle("list-class");
-}
-
-
 function changeHeadline() {
     let headlineElement = document.getElementById("headShow");
-    headlineElement.style.color = "blue";
-    headlineElement.style.fontFamily = "sans-serif";
-    headlineElement.style.fontSize = "2rem";
+    headlineElement.classList.toggle("head-class");
+
 }
 
 function resetHeadline() {
     let headlineElement = document.getElementById("headShow");
-    headlineElement.style.color = ""; 
-    headlineElement.style.fontFamily = "";
-    headlineElement.style.fontSize = "";
+    headlineElement.classList.remove("head-class");
 }
 
 function changeWishes() {
     let wishesElement = document.getElementById("wishes");
-    wishesElement.style.color = "green";
-    wishesElement.style.fontFamily = "sans-serif";
-    wishesElement.style.fontSize = "1.5rem";
+    wishesElement.classList.toggle("list-class");
 }
 
 function resetWishes() {
     let wishesElement = document.getElementById("wishes");
-    wishesElement.style.color = "";
-    wishesElement.style.fontFamily = "";
-    wishesElement.style.fontSize = "";
+    wishesElement.classList.remove("list-class");
 }
