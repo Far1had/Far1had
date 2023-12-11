fcd28552ad3098ee04f14c1412fafb11fcbbd818
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-11 12:55:06 +0100

    CodeFlowUebung-lev1_10

--- Logical-Operator-JS Einfuehrung/assets/js/CodeFlowUebung-lev1_10.js
+++ Logical-Operator-JS Einfuehrung/assets/js/CodeFlowUebung-lev1_10.js
@@ -8,3 +8,12 @@ console.log(z < y || z > y);
 console.log(x === z || x != y);
 console.log(x > 10 && y < 10);
 console.log(x * z <= 100 || x * y > 100);
+
+/*
+CodeFlowUebung-lev1_10.js:5 false
+CodeFlowUebung-lev1_10.js:6 true
+CodeFlowUebung-lev1_10.js:7 true
+CodeFlowUebung-lev1_10.js:8 true
+CodeFlowUebung-lev1_10.js:9 false
+CodeFlowUebung-lev1_10.js:10 true
+*/
