478f4fa84d80e1ada78f943373537af2ebb2a9f5
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-12 00:00:04 +0100

    Conditionals-Level-2_4

--- if_und_else_Statements_lev_2/assets/js/Conditionals-Level-2_4.js
+++ if_und_else_Statements_lev_2/assets/js/Conditionals-Level-2_4.js
@@ -1,1 +1,13 @@
-!
+function berechneUndAusgabeSumme(x, y) {
+    let summe = x + y;
+
+    if (x === y) {
+        summe *= 5;
+        console.log("Beide Zahlen sind gleich. Das Ergebnis ist: " + summe);
+    } else {
+        console.log("Die Summe der beiden Zahlen ist: " + summe);
+    }
+}
+
+berechneUndAusgabeSumme(5, 5);
+
