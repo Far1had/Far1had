49fea1c5404a7eec8c0fbbc2a7534cc59a57eaa7
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-12 14:07:49 +0100

    String-Methods-Level-1_1

---
+++ JS_string_methods_lv_1/assets/js/String-Methods-Level-1_1.js
@@ -0,0 +1,9 @@
+let firstName = "Farhad"
+let lastName = "Amini"
+
+let lengthFirstName = firstName.length;
+let lengthLastName = lastName.length;
+
+console.log(lengthFirstName);
+console.log(lengthLastName);
+
