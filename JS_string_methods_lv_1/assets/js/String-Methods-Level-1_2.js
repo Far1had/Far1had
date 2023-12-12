74a024c2b1309712e2564f6e726b5aaeff7d977d
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-12 14:25:44 +0100

    String-Methods-Level-1_2

--- JS_string_methods_lv_1/assets/js/String-Methods-Level-1_2.js
+++ JS_string_methods_lv_1/assets/js/String-Methods-Level-1_2.js
@@ -1,4 +1,6 @@
 const txt = "How inappropriate to call this planet Earth, when clearly it is Ocean."
-let result = txt.indexOf("h");
+let result = txt.indexOf("Moon");
 
-console.log(result);
+console.log(result); //27
+console.log(result); //38
+console.log(result); //-1
