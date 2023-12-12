02c1d08ad2612a5a9f2c33bc43f3a8ca29736034
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-12 15:46:25 +0100

    String-Methods-Level-1_5

---
+++ JS_string_methods_lv_1/assets/js/String-Methods-Level-1_5.js
@@ -0,0 +1,12 @@
+const text = 'Susi is back from codingschool'
+const result = text.substring(0, 4);
+const result2 = text.substring(5, 7);
+const result3 = text.substring(24, 30);
+const result4 = text.substring(0, 8) + text.substring(24, 30);
+
+
+document.write(result + "<br><br>")
+document.write(result2 + "<br><br>")
+document.write(result3 + "<br><br>")
+document.write(result4 + "<br><br>")
+
