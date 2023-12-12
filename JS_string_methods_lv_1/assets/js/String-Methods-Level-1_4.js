08876de37629414e33ac0292ec9af2efd38bb411
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-12 15:21:07 +0100

    String-Methods-Level-1_

---
+++ JS_string_methods_lv_1/assets/js/String-Methods-Level-1_4.js
@@ -0,0 +1,14 @@
+const A = 'Susi is going to codingschool'
+const result = A.slice(0, 4);
+const result2 = A.slice(5, 7);
+const result3 = A.slice(5, 16) + " " + A.slice( -6);
+const result4 = A.slice(23);
+const result5 = A.slice(0, 4) + " " + A.slice(5, 23) + A.slice(-6);
+
+document.write(result + "<br><br>")
+document.write(result2 + "<br><br>")
+document.write(result3 + "<br><br>")
+document.write(result4 + "<br><br>")
+document.write(result5 + "<br>")
+
+
