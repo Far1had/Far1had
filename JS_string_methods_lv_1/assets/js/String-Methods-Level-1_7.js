fe4878a5793c68cc7112122dd4269a1d0863cd8e
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-12 15:58:13 +0100

    String-Methods-Level-1_7

---
+++ JS_string_methods_lv_1/assets/js/String-Methods-Level-1_7.js
@@ -0,0 +1,8 @@
+const text = "Sam is good at codingschool" 
+let result = text.replace("Sam is good at codingschool", "Sam is bad at school")
+let result2 = text.replace("Sam is good at codingschool", "Susi is good at school")
+let result3 = text.replace("Sam is good at codingschool", "Sam is good at tennis")
+
+document.write(result + "<br><br>")
+document.write(result2 + "<br><br>")
+document.write(result3 + "<br><br>")
