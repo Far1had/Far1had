3449da729bd40e0193e43457ac80237add228a80
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-12 14:34:56 +0100

    String-Methods-Level-1_3

---
+++ JS_string_methods_lv_1/assets/js/String-Methods-Level-1_3.js
@@ -0,0 +1,7 @@
+let txt2 = "Blue, green, grey, white, or black; smooth, ruffled, or mountainous; that ocean is not silent."
+
+let postion = txt2.search("blue")
+
+console.log(postion); // 34
+console.log(postion); // 6
+console.log(postion); // -1
