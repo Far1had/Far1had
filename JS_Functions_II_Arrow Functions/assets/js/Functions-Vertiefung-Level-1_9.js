264cea6a040641938c1f8fc209033ddfa008b478
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-14 13:27:33 +0100

    Functions-Vertiefung-Level-1_9

--- JS_Functions_II_Arrow Functions/assets/js/Functions-Vertiefung-Level-1_9.js
+++ JS_Functions_II_Arrow Functions/assets/js/Functions-Vertiefung-Level-1_9.js
@@ -1,8 +1,10 @@
-const vorstellung = (vorname, nachname, geburtsort, alter, wohnort) => {
-    const ergebnis = `Mein Name ist ${vorname} ${nachname}! Ich bin auf ${geburtsort} geboren. Ich bewache die Welt vor finsteren Mächten. Ich bin ${alter} Jahre alt. Ich wohne in ${wohnort}`
-    console.log(ergebnis);
+function vorstellung (vorname, nachname, geburtsort, alter, wohnort) {
+    const ergebnis = `Mein Name ist ${vorname} ${nachname}! Ich bin auf ${geburtsort} geboren. Ich bewache die Welt vor finsteren Mächten. Ich bin ${alter} Jahre alt. Ich wohne in ${wohnort}`;
+    return ergebnis;
 }
-vorstellung('Anja', 'Engels', 'Offenbach', '29', 'Frankfurt.')
+const final = vorstellung('Anja', 'Engels', 'Offenbach', 29, 'Frankfurt.')
+console.log(final);
+
 
 const vorstellungsTabelle = (vorname, nachname, geburtsort, alter, wohnort) => {
     const ergebnis = `Mein Name ist ${vorname} ${nachname}!
@@ -12,4 +14,4 @@ const vorstellungsTabelle = (vorname, nachname, geburtsort, alter, wohnort) => {
     Ich wohne in ${wohnort}`
     console.log(ergebnis);
 }
-vorstellungsTabelle('Ye', 'Ti', 'Himalaya', '510', 'Tibet.')
+vorstellungsTabelle('Ye', 'Ti', 'Himalaya', 510, 'Tibet.')
