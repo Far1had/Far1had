90e80c3c4205413db95c1857284dfd1b55151e19
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-14 11:29:38 +0100

    Functions-Vertiefung-Level-1_4

---
+++ JS_Functions_II_Arrow Functions/assets/js/Functions-Vertiefung-Level-1_4.js
@@ -0,0 +1,8 @@
+function hero(heroName, heroPower, heroEnemy){
+    const name = ("Mein Lieblingsheld ist: " + heroName)
+    const power = ("Er hat die Fähigkeit: " + heroPower)
+    const enemy = ("Sein größte Gegner war: " + heroEnemy)
+    console.log(name, power, enemy);
+}
+
+hero("Spartakus", "Revolutionär", "Die römische Monarchie")
