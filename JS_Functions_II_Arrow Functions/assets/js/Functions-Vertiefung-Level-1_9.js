d725d2f17d77e7e0f383623c16a89750aa29ec23
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-14 13:08:25 +0100

    Functions-Vertiefung-Level-1_9

---
+++ JS_Functions_II_Arrow Functions/assets/js/Functions-Vertiefung-Level-1_9.js
@@ -0,0 +1,15 @@
+const vorstellung = (vorname, nachname, geburtsort, alter, wohnort) => {
+    const ergebnis = `Mein Name ist ${vorname} ${nachname}! Ich bin auf ${geburtsort} geboren. Ich bewache die Welt vor finsteren Mächten. Ich bin ${alter} Jahre alt. Ich wohne in ${wohnort}`
+    console.log(ergebnis);
+}
+vorstellung('Farhad', 'Amini', 'Teheran', '51', 'Meerbusch.')
+
+const vorstellungsTabelle = (vorname, nachname, geburtsort, alter, wohnort) => {
+    const ergebnis = `Mein Name ist ${vorname} ${nachname}!
+    Ich bin auf ${geburtsort} geboren.
+    Ich bewache die Welt vor finsteren Mächten.
+    Ich bin ${alter} Jahre alt.
+    Ich wohne in ${wohnort}`
+    console.log(ergebnis);
+}
+vorstellungsTabelle('Farhad', 'Amini', 'Teheran', '51', 'Meerbusch.')
