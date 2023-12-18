fb92d8f24e42b06fe1e8954b4eb14874384ec6b2
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-18 12:06:50 +0100

    comitt-Array-Level-1_1

---
+++ arrays1-basics-pop-push-shift-unshift/assets/js/Array-Level-1_1.js
@@ -0,0 +1,6 @@
+let person = ["Farhad", 51, 183]
+let friends = ["Tobi", "Tina", "Uli"]
+let favoriteFood = ["salat", "Bratwurst", "Döner"]
+console.log(person);
+console.log(friends);
+console.log(favoriteFood);
