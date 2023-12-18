b6f91e03111ea710d83952fe77edb2c5f27cebbc
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-18 12:07:35 +0100

    comitt-Array-Level-1_1

--- arrays1-basics-pop-push-shift-unshift/assets/js/Array-Level-1_1.js
+++ arrays1-basics-pop-push-shift-unshift/assets/js/Array-Level-1_1.js
@@ -4,3 +4,7 @@ let favoriteFood = ["salat", "Bratwurst", "Döner"]
 console.log(person);
 console.log(friends);
 console.log(favoriteFood);
+
+/*(3) ['Farhad', 51, 183]
+Array-Level-1_1.js:5 (3) ['Tobi', 'Tina', 'Uli']
+Array-Level-1_1.js:6 (3) ['salat', 'Bratwurst', 'Döner']*/
