4ba961e09ebef19a1ea134a968cfb42db10dd8d9
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-18 12:17:53 +0100

    lvl_1_2_js-vertiefung_arrays_index

---
+++ arrays1-basics-pop-push-shift-unshift/assets/js/ lvl_1_2_js-vertiefung_arrays_index.js
@@ -0,0 +1,40 @@
+let person = ["Farhad", 51, 183]
+let friends = ["Tobi", "Tina", "Uli"]
+let favoriteFood = ["salat", "Bratwurst", "Döner"]
+
+console.log(person);
+console.log(friends);
+console.log(favoriteFood);
+
+//-----------------------*/
+
+console.log(person[0]);
+console.log(person[1]);
+console.log(person[2]);
+
+
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+
+console.log(favoriteFood[0]);
+console.log(favoriteFood[1]);
+console.log(favoriteFood[2]);
+
+/*
+Farhad
+ lvl_1_2_js-vertiefung_arrays_index.js:12 51
+ lvl_1_2_js-vertiefung_arrays_index.js:13 183
+ lvl_1_2_js-vertiefung_arrays_index.js:16 Tobi
+ lvl_1_2_js-vertiefung_arrays_index.js:17 Tina
+ lvl_1_2_js-vertiefung_arrays_index.js:18 Uli
+ lvl_1_2_js-vertiefung_arrays_index.js:20 salat
+ lvl_1_2_js-vertiefung_arrays_index.js:21 Bratwurst
+ lvl_1_2_js-vertiefung_arrays_index.js:22 Döner
+ */
+
+
+
+
+
+
