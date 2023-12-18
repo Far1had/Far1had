016a729ab6943f8687f090d197b8364c690b72f1
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-18 15:38:51 +0100

    Codeflow_Uebung_lev1_9

---
+++ arrays1-basics-pop-push-shift-unshift/assets/js/Codeflow_Uebung_lev1_9.js
@@ -0,0 +1,19 @@
+let laufBand = [23, 54, 75];
+console.log(laufBand);
+
+laufBand.push(1, 15, 20, 28, 44);
+console.log(laufBand);
+
+
+laufBand.unshift(100, 150, 200, 2, 43);
+console.log(laufBand);
+
+laufBand.pop();
+laufBand.pop();
+console.log(laufBand);
+
+laufBand.shift();
+laufBand.shift();
+
+console.log(laufBand);
+
