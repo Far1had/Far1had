9838736d1857e71fa826f81fb128a984141c444f
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-18 12:50:14 +0100

    Array-Level-1_4

---
+++ arrays1-basics-pop-push-shift-unshift/assets/js/Array-Level-1_4.js
@@ -0,0 +1,22 @@
+let person = ["Farhad", 51, 183]
+let friends = ["Tobi", "Tina", "Uli"]
+let favoriteFood = ["salat", "Bratwurst", "Döner"]
+
+let meinDaten = ["Farhad", 51, 183]
+console.log(meinDaten);
+
+console.log(meinDaten);
+
+let pushmeinDaten = meinDaten.push(true, "schwarz")
+console.log(pushmeinDaten);
+
+
+console.log(meinDaten);
+/*
+(3) ['Farhad', 51, 183]
+Array-Level-1_4.js:8 
+(3) ['Farhad', 51, 183]
+Array-Level-1_4.js:11 5
+Array-Level-1_4.js:14 
+(5) ['Farhad', 51, 183, true, 'schwarz']
+*/
