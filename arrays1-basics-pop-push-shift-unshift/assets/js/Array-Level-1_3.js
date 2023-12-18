0a7a644e5ce7a6f91a0c63e44985e05c4e3e1783
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-18 12:25:38 +0100

    Array-Level-1_3

---
+++ arrays1-basics-pop-push-shift-unshift/assets/js/Array-Level-1_3.js
@@ -0,0 +1,7 @@
+let person = ["Farhad", 51, 183]
+let friends = ["Tobi", "Tina", "Uli"]
+let favoriteFood = ["salat", "Bratwurst", "Döner"]
+
+console.log(person.length); // 3
+console.log(friends.length); // 3
+console.log(favoriteFood.length); // 3
