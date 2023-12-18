4b54511ceaff84c3bd2c64f6f822f65070d986e3
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-18 11:56:07 +0100

    comitt-Array-aufgaben

---
+++ tag29/js/main.js
@@ -0,0 +1,113 @@
+// * String Number Boolean
+
+const string = "ich  bin ein String"
+const number = 245
+const boolean = true
+
+// * Array       0       1      2       3        4
+let team = ["Alex", "John", "Kim", "Jimmy", "Tommy"]
+console.log(team);
+
+let player1 = "Alex";
+let player2 = "John";
+let player3 = "Kim";
+
+
+let teamArr = [player1, player2, player3]
+console.log(teamArr);
+
+// * index
+// So können wir auf einzelne elemente zugreifen, indem wir eine Zahl in eine eckige Klammer reinschreiben
+
+
+console.log(team[1]); // John
+console.log(team[4]); // Tommy
+
+// * indexOf()
+// sucht in einem Array nach dem suchwort gib uns den Index zurrück
+
+console.log(team.indexOf("John")); // 1
+console.log(team.indexOf("Jimmy")); // 3
+
+
+team[0] = "Julia"
+team[2] = "Daniel"
+
+console.log(team);
+
+// Dynamisch
+console.log(team.indexOf("Julia")); // index = 0
+team[team.indexOf("Julia")] = "Nina"
+console.log(team);
+
+const searchUser = () => {
+    let user = document.querySelector('#user').value
+    console.log(user);
+    let newUser = document.querySelector('#newUser').value
+    console.log(newUser);
+
+
+    if (team.includes(user)) {
+        team[team.indexOf(user)] = newUser;
+        console.log("danach", team);
+    } else {
+        console.log("User nicht gefunden");
+    }
+}
+console.log(team);
+
+
+// * length
+console.log(team.length); // 5
+
+
+// Just good to know
+console.log(team[team.length - 2]);
+
+
+let mitarbeiter = ["jimmy", "Johanna", "Christian", "Jannik", "Steffen"]
+console.log(mitarbeiter);
+
+// * pop()
+// Die pop() Methode entfernt das letze Element in einem Array und gibt den Wert, des gelöschtes Elements zurrück
+console.log("%c -------pop--------", "color: red")
+// Steffen
+let popMitarbeiter = mitarbeiter.pop()
+console.log(popMitarbeiter);
+
+console.log(mitarbeiter);
+
+console.log("%c -------shift--------", "color: red")
+// * shift()
+// die Shift() Methode entfernt das erste Element in einem Array und gibt den Wert, des gelöschtes Elements zurrück
+let shiftMitarbeiter = mitarbeiter.shift()
+console.log(shiftMitarbeiter);
+
+console.log(mitarbeiter);
+
+
+console.log("%c -------push--------", "color: red")
+// * push()
+
+// die push() Methode  fügt an der letzen Stelle ein Element hinzu und gibt uns die neue länge von dem Array zurrück
+
+let pushMitarbeiter = mitarbeiter.push("Lisa")
+console.log(pushMitarbeiter);
+console.log(mitarbeiter);
+
+
+// const einStellen = () => {
+//     let neueMitarbeiter = document.querySelector("#neueMitarbeiter").value
+//     mitarbeiter.push(neueMitarbeiter)
+// }
+
+console.log("%c -------unshift--------", "color: red")
+// * unshift()
+// die unshift() Methode fügt an der erste Stelle ein Element hinzu und gibt uns die neue länge von dem Array zurrück
+
+let unshiftMitarbeiter = mitarbeiter.unshift("Timo")
+console.log(unshiftMitarbeiter);
+console.log(mitarbeiter);
+
+
+
