dacdcdfe13b5295c355eb1ce08aebe44c71e1c5c
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-19 18:35:56 +0100

    Array-Level-1_10

---
+++ Arrays_II_(slice_split)/assets/js/Array-Level-1_10.js
@@ -0,0 +1,8 @@
+const text = "It's okay, Bender. I like cooking too. Oh, you're a dollar naughtier than most. My fellow Earthicans, as I have explained in my book 'Earth in the Balance', and the much more popular 'Harry Potter and the Balance of Earth', we need to defend our planet against pollution. Also dark wizards.";
+
+const words = text.split(' ');
+console.log(words);
+
+const chars = text.split('');
+console.log(chars);
+
