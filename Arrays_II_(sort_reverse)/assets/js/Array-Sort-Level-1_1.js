cc451be92e1695602b8265ff1eb377bb5cf019a8
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-19 12:39:46 +0100

    Array-Sort-Level-1_1

---
+++ Arrays_II_(sort_reverse)/assets/js/Array-Sort-Level-1_1.js
@@ -0,0 +1,20 @@
+let languages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "Ruby",
+    "PHP",
+    "C++",
+    "CSS",
+    "C#",
+    "Go",
+    "C",
+    "TypeScript",
+    "Swift"
+];
+languages.sort();
+console.log(languages);
+
+/*
+(12) ['C', 'C#', 'C++', 'CSS', 'Go', 'Java', 'JavaScript', 'PHP', 'Python', 'Ruby', 'Swift', 'TypeScript']
+*/
