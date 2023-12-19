a33cbe2b84405cdaf0a02183e2be5ac0671519b5
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-19 18:24:53 +0100

    AArray-Level-1_8

---
+++ Arrays_II_(slice_split)/assets/js/Array-Level-1_8.js
@@ -0,0 +1,8 @@
+let lieblingsreiseziele = ["Teheran", "Istanbul", "Rio", "Tokio", "Rey"];
+
+console.log("Vor dem Slicen:", lieblingsreiseziele);
+
+let extrahierteZiele = lieblingsreiseziele.slice(1, 3);
+
+console.log("Nach dem Slicen:", lieblingsreiseziele);
+console.log("Extrahierte Ziele:", extrahierteZiele);
