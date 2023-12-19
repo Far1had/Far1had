1d75a8897209fc154c17ba452267ba85a8e2e4e2
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-19 12:51:33 +0100

    Array-Sort-Level-1_2

---
+++ Arrays_II_(sort_reverse)/assets/js/Array-Sort-Level-1_2.js
@@ -0,0 +1,28 @@
+let sortierung2 = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "Ruby",
+    "PHP",
+    "C++",
+    "CSS",
+    "C#",
+    "Go",
+    "C",
+    "TypeScript",
+    "Swift"
+];
+
+console.log(sortierung2);
+
+sortierung2.sort();
+console.log(sortierung2);
+
+sortierung2.reverse();
+console.log(sortierung2);
+
+/*
+2) ['JavaScript', 'Python', 'Java', 'Ruby', 'PHP', 'C++', 'CSS', 'C#', 'Go', 'C', 'TypeScript', 'Swift']
+Array-Sort-Level-1_2.js:19 (12) ['C', 'C#', 'C++', 'CSS', 'Go', 'Java', 'JavaScript', 'PHP', 'Python', 'Ruby', 'Swift', 'TypeScript']
+Array-Sort-Level-1_2.js:22 (12) ['TypeScript', 'Swift', 'Ruby', 'Python', 'PHP', 'JavaScript', 'Java', 'Go', 'CSS', 'C++', 'C#', 'C']
+*/
