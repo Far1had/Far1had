22431dea3729d6683b6b25e195140b4240a0c6ba
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-19 14:39:00 +0100

    Array-Sort-Level-2_2

---
+++ Arrays_II_(sort_reverse)/assets/js/Array-Sort-Level-2_2.js
@@ -0,0 +1,18 @@
+let numArray2 = [36, 324, 122, 62, 98, 88, 99, 1000];
+
+console.log(numArray2);
+numArray2.forEach((pet, index) => {
+    console.log(index + ": " + pet + "❤️");
+});
+
+/*
+(8) [36, 324, 122, 62, 98, 88, 99, 1000]
+Array-Sort-Level-2_2.js:5 0: 36❤️
+Array-Sort-Level-2_2.js:5 1: 324❤️
+Array-Sort-Level-2_2.js:5 2: 122❤️
+Array-Sort-Level-2_2.js:5 3: 62❤️
+Array-Sort-Level-2_2.js:5 4: 98❤️
+Array-Sort-Level-2_2.js:5 5: 88❤️
+Array-Sort-Level-2_2.js:5 6: 99❤️
+Array-Sort-Level-2_2.js:5 7: 1000❤️
+*/
