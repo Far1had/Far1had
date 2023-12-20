c4ab35d3242869bcf6fce14eb07d7af92af7be7b
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-20 13:40:07 +0100

    Loops-Level-1_4

---
+++ JS_Loops_Lev_I/assets/js/Loops-Level-1_4.js
@@ -0,0 +1,13 @@
+let names = ["Georg", "Anass", "Elaine", "Hakan", "Eric", "Kim", "Sergio"];
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
+// Georg
+// Loops-Level-1_4.js:4 Anass
+// Loops-Level-1_4.js:4 Elaine
+// Loops-Level-1_4.js:4 Hakan
+// Loops-Level-1_4.js:4 Eric
+// Loops-Level-1_4.js:4 Kim
+// Loops-Level-1_4.js:4 Sergio
