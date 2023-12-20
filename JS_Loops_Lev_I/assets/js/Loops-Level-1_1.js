dc62d3869b2ca181d6ab40adf456abac4d0bff0d
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-20 13:05:07 +0100

    Loops-Level-1_1

---
+++ JS_Loops_Lev_I/assets/js/Loops-Level-1_1.js
@@ -0,0 +1,36 @@
+console.log("%c 1 bis 10", "color: green");
+
+for (let i = 1; i <= 10; i++) {
+    console.log("Hello World " + i);
+}
+
+console.log("%c 10 bis 20", "color: red");
+
+for (let i = 10; i <= 20; i++) {
+    console.log("Hello World " + i);
+}
+
+
+// 1 bis 10
+// Loops-Level-1_1.js:4 Hello World 1
+// Loops-Level-1_1.js:4 Hello World 2
+// Loops-Level-1_1.js:4 Hello World 3
+// Loops-Level-1_1.js:4 Hello World 4
+// Loops-Level-1_1.js:4 Hello World 5
+// Loops-Level-1_1.js:4 Hello World 6
+// Loops-Level-1_1.js:4 Hello World 7
+// Loops-Level-1_1.js:4 Hello World 8
+// Loops-Level-1_1.js:4 Hello World 9
+// Loops-Level-1_1.js:4 Hello World 10
+// Loops-Level-1_1.js:7  10 bis 20
+// Loops-Level-1_1.js:10 Hello World 10
+// Loops-Level-1_1.js:10 Hello World 11
+// Loops-Level-1_1.js:10 Hello World 12
+// Loops-Level-1_1.js:10 Hello World 13
+// Loops-Level-1_1.js:10 Hello World 14
+// Loops-Level-1_1.js:10 Hello World 15
+// Loops-Level-1_1.js:10 Hello World 16
+// Loops-Level-1_1.js:10 Hello World 17
+// Loops-Level-1_1.js:10 Hello World 18
+// Loops-Level-1_1.js:10 Hello World 19
+// Loops-Level-1_1.js:10 Hello World 20
