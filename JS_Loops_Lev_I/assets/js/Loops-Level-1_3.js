9b5440ff77961bdaa8be08499e920c1a73d49775
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-20 13:33:33 +0100

    Loops-Level-1_3

---
+++ JS_Loops_Lev_I/assets/js/Loops-Level-1_3.js
@@ -0,0 +1,18 @@
+
+let counter = 0;
+
+while (counter < 10) {
+    console.log("Hallo World " + counter);
+    counter++;
+}
+
+// Hallo World 0
+// Loops-Level-1_3.js:5 Hallo World 1
+// Loops-Level-1_3.js:5 Hallo World 2
+// Loops-Level-1_3.js:5 Hallo World 3
+// Loops-Level-1_3.js:5 Hallo World 4
+// Loops-Level-1_3.js:5 Hallo World 5
+// Loops-Level-1_3.js:5 Hallo World 6
+// Loops-Level-1_3.js:5 Hallo World 7
+// Loops-Level-1_3.js:5 Hallo World 8
+// Loops-Level-1_3.js:5 Hallo World 9
