8867709e4b38c8578ee2bacfec0b083f2773b92e
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-20 13:25:35 +0100

    Loops-Level-1_2

--- JS_Loops_Lev_I/assets/js/Loops-Level-1_2.js
+++ JS_Loops_Lev_I/assets/js/Loops-Level-1_2.js
@@ -1,1 +1,12 @@
-(let numArray = \[\];)
+let numArray = [];
+for (let i = 0; i <= 10; i++) {
+    numArray.push(i)
+}
+console.log(numArray);
+
+
+
+console.log("%c Ein Array mit den Zahlen 0-10 mithilfe einer Schleife erstellt!", "color: green");
+
+// Ein Array mit den Zahlen 0-10 mithilfe einer Schleife erstellt!
+// Loops-Level-1_2.js:8 (11) [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
