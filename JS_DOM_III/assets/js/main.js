272650303579c7568f2667475b3dfe4b7b0e99cd
Farhad <149811241+Far1had@users.noreply.github.com>
2023-12-21 13:50:21 +0100

    Dom-Elements-Level-1_1

---
+++ JS_DOM_III/assets/js/main.js
@@ -0,0 +1,31 @@
+function addElement() {
+    // Eingabefeld und Liste abrufen
+    let inputElement = document.getElementById('inputText');
+    let list = document.getElementById('myList');
+
+    // Eingabe überprüfen
+    let inputValue = inputElement.value; // Leerzeichen entfernen
+    if (inputValue === '') {
+        // Zeige eine Meldung im Dokument an
+        errorMessage.textContent = 'Bitte geben Sie einen gültigen Wert ein.';
+        return;
+    }
+
+    // Neues Listenelement erstellen
+    let listItem = document.createElement('li');
+    listItem.textContent = inputValue;
+
+    // Listenelement zur Liste hinzufügen
+    list.appendChild(listItem);
+
+    // Eingabefeld leeren
+    inputElement.value = '';
+    errorMessage.textContent = '';
+}
+document.getElementById('inputText').addEventListener('keydown', function(event) {
+    // Wenn die gedrückte Taste die Enter-Taste ist
+    if (event.key === 'Enter') {
+        // Führe die addElement-Funktion aus
+        addElement();
+    }
+});
