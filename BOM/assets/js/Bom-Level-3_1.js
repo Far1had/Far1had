44c9cb6f5534687918c5523fba0a8b8af10068ab
Farhad <149811241+Far1had@users.noreply.github.com>
2024-01-04 14:38:38 +0100

    1commit

--- BOM/assets/js/Bom-Level-3_1.js
+++ BOM/assets/js/Bom-Level-3_1.js
@@ -2,7 +2,7 @@ let catElement = document.getElementById("cat");
 let intervalId;
 let isWalking = false;
 let direction = 1; 
-let speed = 6; 
+let speed = 1; 
 
 function catWalk() {
     if (!isWalking) {
@@ -38,6 +38,14 @@ function catSpeed() {
     speed += 2; 
 }
 
+
 function changeSpeed(newSpeed) {
-    speed = newSpeed;
+    // Überprüfe, ob die neue Geschwindigkeit kleiner als 1 ist
+    if (newSpeed > 1) {
+        // Setze die Geschwindigkeit auf 1, wenn sie kleiner als 1 ist
+        speed = 1;
+    } else {
+        // Andernfalls setze die Geschwindigkeit auf den angegebenen Wert
+        speed = newSpeed;
+    }
 }
