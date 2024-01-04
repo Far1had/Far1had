9a645369d052773b457e68a49cf34e11069175eb
Farhad <149811241+Far1had@users.noreply.github.com>
2024-01-04 12:16:18 +0100

    1commit

--- BOM/assets/js/Bom-Level-2_1.js
+++ BOM/assets/js/Bom-Level-2_1.js
@@ -8,7 +8,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (count === 0) {
             clearInterval(intervalId);
-            countElement.parentElement.style.display = "none";
+            
+            // Füge eine CSS-Animation hinzu, um die Transparenz allmählich zu reduzieren
+            countElement.parentElement.style.animation = "fadeOut 0.5s ease-out forwards";
+            
+            // Nach der Animation kannst du weitere Aktionen hinzufügen
+            countElement.parentElement.addEventListener("animationend", function() {
+                countElement.parentElement.style.display = "none";
+                // Hier können weitere Aktionen ausgeführt werden, wenn das Element ausgeblendet ist
+            });
         } else {
             count--; 
         }
